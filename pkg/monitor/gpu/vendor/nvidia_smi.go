f9dd6f6caa9c429ee295d8ff0873f4b29713a048
kuratajr <n19dcat085@student.ptithcm.edu.vn>
2026-04-21 10:38:05 +0700

    feat: initialize project structure and github workflows

---
+++ pkg/monitor/gpu/vendor/nvidia_smi.go
@@ -0,0 +1,120 @@
+package vendor
+
+// Modified from https://github.com/influxdata/telegraf/blob/master/plugins/inputs/nvidia_smi/nvidia_smi.go
+// Original License: MIT
+
+import (
+	"encoding/xml"
+	"errors"
+	"os"
+	"os/exec"
+	"strconv"
+	"strings"
+	"sync"
+)
+
+var (
+	nvidiaSmiPath string
+	nvidiaSmiOnce sync.Once
+)
+
+type NvidiaSMI struct {
+	BinPath string
+	data    []byte
+}
+
+func (smi *NvidiaSMI) GatherModel() ([]string, error) {
+	return smi.gatherModel()
+}
+
+func (smi *NvidiaSMI) GatherUsage() ([]float64, error) {
+	return smi.gatherUsage()
+}
+
+func (smi *NvidiaSMI) Start() error {
+	nvidiaSmiOnce.Do(func() {
+		if _, err := os.Stat(smi.BinPath); err == nil {
+			nvidiaSmiPath = smi.BinPath
+		} else {
+			if binPath, err := exec.LookPath("nvidia-smi"); err == nil {
+				nvidiaSmiPath = binPath
+			}
+		}
+	})
+
+	if nvidiaSmiPath == "" {
+		return errors.New("didn't find the adequate tool to query GPU utilization")
+	}
+
+	smi.BinPath = nvidiaSmiPath
+	smi.data = smi.pollNvidiaSMI()
+	return nil
+}
+
+func (smi *NvidiaSMI) pollNvidiaSMI() []byte {
+	cmd := exec.Command(smi.BinPath,
+		"-q",
+		"-x",
+	)
+	gs, err := cmd.CombinedOutput()
+	if err != nil {
+		return nil
+	}
+	return gs
+}
+
+func (smi *NvidiaSMI) gatherModel() ([]string, error) {
+	var s smistat
+	var models []string
+
+	err := xml.Unmarshal(smi.data, &s)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, gpu := range s.GPUs {
+		models = append(models, gpu.ProductName)
+	}
+
+	return models, nil
+}
+
+func (smi *NvidiaSMI) gatherUsage() ([]float64, error) {
+	var s smistat
+	var percentage []float64
+
+	err := xml.Unmarshal(smi.data, &s)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, gpu := range s.GPUs {
+		gp, _ := parsePercentage(gpu.Utilization.GpuUtil)
+		percentage = append(percentage, gp)
+	}
+
+	return percentage, nil
+}
+
+func parsePercentage(p string) (float64, error) {
+	per := strings.ReplaceAll(p, " ", "")
+
+	t := strings.TrimSuffix(per, "%")
+
+	value, err := strconv.ParseFloat(t, 64)
+	if err != nil {
+		return 0, err
+	}
+
+	return value, nil
+}
+
+type gpu struct {
+	ProductName string `xml:"product_name"`
+	Utilization struct {
+		GpuUtil string `xml:"gpu_util"`
+	} `xml:"utilization"`
+}
+type smistat struct {
+	GPUs []gpu `xml:"gpu"`
+}
